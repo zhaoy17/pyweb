18f38fdff7138975f392189447e2b5d48b885cda
Peter Zhao <peterzhao204@gmail.com>
2020-08-20 02:15:24 -0400

    added more functionalities

--- pyweb/http/httpRequest.py
+++ pyweb/http/httpRequest.py
@@ -1,8 +1,10 @@
+from typing import Callable
 from .url import parse_query_string, parse_host, parse_path_info
+from .header import parse_content_type, types_map
 
 
 class HTTPRequest:
-    def __init__(self, environ):
+    def __init__(self, environ: dict):
         self._env = environ
         self.method = environ["REQUEST_METHOD"]
         try:
@@ -18,7 +20,7 @@ class HTTPRequest:
         self._content_type = None
 
     @property
-    def query(self):
+    def query(self) -> str:
         try:
             raw_string = self._env["QUERY_STRING"] \
                 .encode("latin1").decode("utf-8", "replace")
@@ -27,20 +29,17 @@ class HTTPRequest:
         return raw_string
 
     @property
-    def params(self):
-        if not self._query_string:
+    def params(self) -> dict:
+        if self._query_string is None:
             self._query_string = parse_query_string(self._env["QUERY_STRING"])
         return self._query_string
 
     @property
-    def object_location(self):
+    def object_location(self) -> list:
         if self._object_location is None:
             self._object_location = parse_path_info(self._pathinfo)
         else:
             return self._object_location
-
-    '''Return the port According to PEP3333, HTTP_HOST is present should be preferred over
-    SEVER_NAME and SEVER_PORT variable'''
 
     @property
     def port(self):
@@ -62,12 +61,43 @@ class HTTPRequest:
         return self._port
 
     @property
-    def host(self):
+    def host(self) -> str:
         if self._host is None:
             self.port()
         return self._host
 
     @property
-    def content_type(self):
+    def content_type(self) -> list:
         if self.method == "GET":
+            return ["", {}]
+        if self._content_type is None:
+            try:
+                header = self._env["CONTENT_TYPE"]
+            except KeyError:
+                header = ""
+            self._content_type = parse_content_type(header)
+        return self._content_type
+
+    @property
+    def content_length(self) -> int:
+        try:
+            return self._env['CONTENT_LENGTH']
+        except KeyError:
+            return -1
+
+    @property
+    def content_extension(self) -> str:
+        try:
+            return types_map[self.content_type[0]]
+        except KeyError:
             return ""
+
+    def get_header(self, header: str, parser: Callable = None) -> str:
+        try:
+            output_header = self._env["HTTP_" + header.strip().upper()]
+        except KeyError:
+            raise ValueError("requires header: {}".format(header.strip().upper()))
+        if parser is not None:
+            return parser(output_header)
+        else:
+            return output_header
